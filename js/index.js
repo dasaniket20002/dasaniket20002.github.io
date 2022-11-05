52c1b35de13fedd244a837de5750a98bc3dfb615
Aniket Das <47720941+dasaniket20002@users.noreply.github.com>
2022-11-05 17:02:34 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,21 +2,21 @@ var intro = document.querySelectorAll(".intro-container .intro h1");
 var time = 150;
 const num_languages = intro.length;
 
-var i = 0;
+var lang_index = 0;
 
 function intro_play() {
-	i++;
+	lang_index++;
 
-	if (num_languages - i == 9) time += 25;
-	if (num_languages - i == 7) time += 50;
-	if (num_languages - i == 5) time += 50;
+	if (num_languages - lang_index == 9) time += 25;
+	if (num_languages - lang_index == 7) time += 50;
+	if (num_languages - lang_index == 5) time += 50;
 
-	if (i < num_languages) {
+	if (lang_index < num_languages) {
 		setTimeout(intro_play, time);
-		intro[i - 1].classList.remove("active");
-		intro[i].classList.add("active");
+		intro[lang_index - 1].classList.remove("active");
+		intro[lang_index].classList.add("active");
 	} else {
-		intro[i - 1].classList.add("active");
+		intro[lang_index - 1].classList.add("active");
 		document.querySelector(".intro").classList.add("exit");
 		setTimeout(function () {
 			document.querySelector(".intro-container").classList.add("exit");
@@ -27,6 +27,9 @@ function intro_play() {
 	}
 }
 setTimeout(intro_play, time);
+
+document.getElementById("set_year").innerHTML =
+	"&#169;" + new Date().getFullYear();
 
 var skill_list = document.querySelectorAll(
 	".about-container .about .skills ul li"
@@ -46,3 +49,14 @@ function handleClick(i) {
 		skill_list[j].style.fontSize = "1.75vh";
 	}
 }
+
+const anim_texts = ["GET IN TOUCH", "LET ME KNOW", "LET'S HAVE A CHAT"];
+let form_anim_index = 0;
+var form_anim_element = document.querySelector(
+	".form-container form section h1 span"
+);
+function form_anim() {
+	form_anim_element.innerHTML = anim_texts[form_anim_index];
+	form_anim_index = (form_anim_index + 1) % anim_texts.length;
+}
+setInterval(form_anim, 7000);
