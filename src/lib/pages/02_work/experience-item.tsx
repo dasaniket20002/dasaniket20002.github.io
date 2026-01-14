193fb0d409af6d81ed46e74abfcc3681db6b90bd
Aniket Das <dasaniket20002@gmail.com>
2026-01-14 17:00:54 +0530

    reworked experience section, with morphing dialog

--- src/lib/pages/02_work/experience-item.tsx
+++ src/lib/pages/02_work/experience-item.tsx
@@ -1,77 +1,153 @@
-import { motion, useMotionValue, type HTMLMotionProps } from "motion/react";
-import { forwardRef } from "react";
+import {
+  IconArrowForward,
+  IconCalendar,
+  IconExternalLink,
+  IconLocation,
+} from "@tabler/icons-react";
+import { motion, stagger } from "motion/react";
+import HorizontalDivider from "../../components/horizontal-divider";
+import {
+  MorphingDialog,
+  MorphingDialogClose,
+  MorphingDialogContainer,
+  MorphingDialogContent,
+  MorphingDialogDescription,
+  MorphingDialogImage,
+  MorphingDialogSubtitle,
+  MorphingDialogTitle,
+  MorphingDialogTrigger,
+} from "../../components/morphing-dialog";
+import TextRoll from "../../components/text-roll";
 import { cn } from "../../utils";
 
-const ExperienceItem = forwardRef<
-  HTMLDivElement,
-  {
-    className?: string;
-    imgClassName?: string;
-    url: string;
-    alt: string;
-    title: string;
-    company: string;
-    location: string;
-    period: string;
-  } & HTMLMotionProps<"div">
->(
-  (
-    {
-      className,
-      imgClassName,
-      url,
-      alt,
-      title,
-      company,
-      location,
-      period,
-      ...motionProps
-    },
-    ref
-  ) => {
-    const color = useMotionValue("var(--color-dark-1)");
-    return (
-      <motion.div
-        ref={ref}
-        {...motionProps}
+const ExperienceItem = ({
+  className,
+  imgClassName,
+  imgSrc,
+  imgAlt,
+  title,
+  company,
+  url,
+  location,
+  period,
+  description,
+}: {
+  className?: string;
+  imgClassName?: string;
+  imgSrc: string;
+  imgAlt: string;
+  title: string;
+  company: string;
+  url: string;
+  location: string;
+  period: string;
+  description: string[];
+}) => {
+  return (
+    <MorphingDialog>
+      <MorphingDialogTrigger
         className={cn(
-          "grid grid-cols-subgrid col-span-full size-min p-4 rounded-lg hover:bg-dark-1/10 cursor-pointer",
+          "group flex flex-col gap-4 rounded-xl transition-[shadow,color,border] px-3 py-3 hover:shadow-2xl border border-transparent hover:border-light-2/10",
           className
         )}
-        onMouseEnter={() =>
-          color.set(
-            "color-mix(in oklch, var(--color-light-2) 80%, transparent)"
-          )
-        }
-        onMouseLeave={() => color.set("var(--color-dark-1)")}
       >
-        <motion.div
-          className={cn(
-            "size-16 p-2 rounded flex items-center justify-center transition-colors delay-100",
-            imgClassName
-          )}
-          style={{ backgroundColor: color }}
-        >
-          <img src={url} alt={alt} className="w-full" />
-        </motion.div>
-        <section className="-space-y-1 place-content-center col-span-2 md:col-span-1 tracking-wide">
-          <h3 className="font-think-loved text-xl md:text-2xl text-light-2">
-            {title}
-          </h3>
-          <motion.p style={{ color }} className="transition-colors delay-100">
-            {company}
-          </motion.p>
-        </section>
-        <motion.section
-          style={{ color }}
-          className="italic font-light place-content-center col-span-full md:col-span-1 transition-colors delay-100 tracking-wider"
-        >
-          <p className="text-xs text-end">{location}</p>
-          <p className="text-end">{period}</p>
-        </motion.section>
-      </motion.div>
-    );
-  }
-);
+        <div className="flex gap-4 items-center">
+          <div
+            className={cn(
+              "bg-dark-1 aspect-square w-16 rounded-lg place-items-center place-content-center flex-none",
+              imgClassName
+            )}
+          >
+            <MorphingDialogImage
+              src={imgSrc}
+              alt={imgAlt}
+              className="drop-shadow-xl"
+            />
+          </div>
+          <div>
+            <MorphingDialogTitle className="font-think-loved text-3xl tracking-wide whitespace-nowrap">
+              {title}
+            </MorphingDialogTitle>
+            <MorphingDialogSubtitle className="font-thin tracking-wider text-start">
+              {company}
+            </MorphingDialogSubtitle>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1 text-dark-1 tracking-wide group-hover:text-light-2 transition-colors">
+          <div className="flex gap-4 items-center">
+            <span className="w-16 place-items-center">
+              <IconLocation className="size-4" />
+            </span>
+            <p>{location}</p>
+          </div>
+          <div className="flex gap-4 items-center">
+            <span className="w-16 place-items-center">
+              <IconCalendar className="size-4" />
+            </span>
+            <p>{period}</p>
+          </div>
+        </div>
+      </MorphingDialogTrigger>
+
+      <MorphingDialogContainer>
+        <MorphingDialogContent className="p-6 pointer-events-auto relative flex h-auto w-full flex-col gap-6 overflow-hidden rounded-xl shadow-2xl border border-light-2/10 bg-dark-2 sm:w-lg">
+          <div className="flex gap-6 items-center">
+            <div
+              className={cn(
+                "bg-dark-1 aspect-square w-16 rounded-lg place-items-center place-content-center",
+                imgClassName
+              )}
+            >
+              <MorphingDialogImage
+                src={imgSrc}
+                alt={imgAlt}
+                className="drop-shadow-xl"
+              />
+            </div>
+            <div className="text-dark-1">
+              <MorphingDialogTitle className="font-think-loved text-3xl tracking-wide whitespace-nowrap">
+                {title}
+              </MorphingDialogTitle>
+              <MorphingDialogSubtitle className="tracking-wide text-start">
+                {company}
+              </MorphingDialogSubtitle>
+            </div>
+          </div>
+          <HorizontalDivider />
+          <MorphingDialogDescription
+            disableLayoutAnimation
+            variants={{
+              initial: { opacity: 0, scale: 0.8, y: 100 },
+              animate: { opacity: 1, scale: 1, y: 0 },
+              exit: { opacity: 0, scale: 0.8, y: 100 },
+            }}
+            transition={{ delayChildren: stagger(0.5) }}
+            className="grid grid-cols-[1rem_1fr] gap-x-4 gap-y-2 tracking-wider text-light-2"
+          >
+            {description.map((d, i) => (
+              <motion.div
+                key={i}
+                className="grid grid-cols-subgrid col-span-full"
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                <IconArrowForward className="size-4 mt-0.5 stroke-1" />
+                <p>{d}</p>
+              </motion.div>
+            ))}
+            <div className="grid grid-cols-subgrid col-span-full items-center pt-6">
+              <IconExternalLink className="size-4 stroke-1" />
+              <a href={url} className="text-xs font-thin tracking-wider w-min">
+                <TextRoll>{company}</TextRoll>
+              </a>
+            </div>
+          </MorphingDialogDescription>
+          <MorphingDialogClose className="text-light-2" />
+        </MorphingDialogContent>
+      </MorphingDialogContainer>
+    </MorphingDialog>
+  );
+};
 
 export default ExperienceItem;
--- src/lib/pages/02_work/experience.tsx
+++ src/lib/pages/02_work/experience.tsx
@@ -1,7 +1,5 @@
 import { motion, type HTMLMotionProps } from "motion/react";
-import { forwardRef, useRef } from "react";
-import HorizontalDivider from "../../components/horizontal-divider";
-import { useElementSize } from "../../hooks/use-element-size";
+import { forwardRef } from "react";
 import { cn } from "../../utils";
 import ExperienceItem from "./experience-item";
 
@@ -9,152 +7,129 @@ const Experience = forwardRef<
   HTMLElement,
   { className?: string } & HTMLMotionProps<"section">
 >(({ className, ...motionProps }, ref) => {
-  const cubesContainerRef = useRef<HTMLDivElement>(null);
-  const cubesContainerSize = useElementSize(cubesContainerRef);
   return (
     <motion.section
+      ref={ref}
+      {...motionProps}
       className={cn(
-        "bg-dark-2 min-h-[calc(100vh-var(--head-height))] px-8 md:px-32 flex flex-col gap-8",
+        "place-content-center place-items-center h-[calc(100vh-var(--head-height))] px-8 py-1 @container-[size]",
         className
       )}
-      {...motionProps}
-      ref={ref}
     >
-      <h2 className="col-span-full text-4xl md:text-[min(5vw,4.5rem)] font-think-loved text-light-2 trim-text-caps py-8 text-center flex-none">
-        {["E", "X", "P", "E", "RI", "E", "N", "C", "E"].map((it, i) => (
-          <motion.span
-            initial={{ scale: 1.5, width: 0, opacity: 0 }}
-            whileInView={{ scale: 1, width: "auto", opacity: 1 }}
-            transition={{ ease: "backOut", delay: i * 0.02 }}
-            viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-            key={i}
-            className="inline-block"
-          >
-            {it}
-          </motion.span>
-        ))}
-      </h2>
-
-      <div className="w-full flex gap-32 items-center justify-center">
-        <motion.section
-          initial={{ scale: 1.05, opacity: 0 }}
-          whileInView={{ scale: 1, opacity: 1 }}
-          transition={{ ease: "backOut", delay: 0.25, duration: 1 }}
-          viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-          className="h-[20vh] lg:h-[40vh] lg:py-12"
-        >
-          <img
-            src="assets/illustrations/undraw_work-together_0f8c.svg"
-            alt="work together illustration"
-            className="size-full aspect-auto"
-          />
-        </motion.section>
-
-        <section className="py-12 grid grid-cols-[1fr_5fr_3fr] auto-rows-min gap-x-4 gap-y-3 self-center flex-1">
-          <HorizontalDivider
-            className="mx-4 col-span-full"
-            initial={{ clipPath: "inset(0% 50% 0% 50%)" }}
-            whileInView={{ clipPath: "inset(0% 0% 0% 0%)" }}
-            transition={{ ease: "backOut", delay: 0.5, duration: 1 }}
-            viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-          />
-
-          <ExperienceItem
-            url={"assets/logos/JMAN Group.png"}
-            alt={"JMAN Group"}
-            title={"Software Engineer"}
-            company={"JMAN Group Pvt.Ltd."}
-            location={"Chennai, India (On-Site)"}
-            period={"Jul/2024 - Sep/2025"}
-            initial={{ y: 24, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1, transition: { delay: 0.25 } }}
-            whileHover={{
-              scale: 1.02,
-              transition: { delay: 0, ease: "anticipate" },
-            }}
-            transition={{ ease: "backOut", duration: 0.5 }}
-            viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-          />
-
-          <HorizontalDivider
-            className="mx-4 col-span-full"
-            initial={{ clipPath: "inset(0% 50% 0% 50%)" }}
-            whileInView={{ clipPath: "inset(0% 0% 0% 0%)" }}
-            transition={{ ease: "backOut", delay: 0.5, duration: 1 }}
-            viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-          />
-
-          <ExperienceItem
-            imgClassName="p-4"
-            url={"assets/logos/IDZ Digital.png"}
-            alt={"IDZ Digital"}
-            title={"Game Developer"}
-            company={"IDZ Digital Pvt.Ltd."}
-            location={"Mumbai, India (Remote)"}
-            period={"Jun/2023 - Oct/2023"}
-            initial={{ y: 24, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1, transition: { delay: 0.25 } }}
-            whileHover={{
-              scale: 1.02,
-              transition: { delay: 0, ease: "anticipate" },
-            }}
-            transition={{ ease: "backOut", duration: 0.5 }}
-            viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-          />
-
-          <HorizontalDivider
-            className="mx-4 col-span-full"
-            initial={{ clipPath: "inset(0% 50% 0% 50%)" }}
-            whileInView={{ clipPath: "inset(0% 0% 0% 0%)" }}
-            transition={{ ease: "backOut", delay: 0.5, duration: 1 }}
-            viewport={{ once: true, margin: "0% 0% -30% 0%" }}
-          />
-        </section>
-      </div>
-
-      <motion.div
-        initial={{ scale: 1.05, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1 }}
-        transition={{ ease: "backOut", delay: 0.25, duration: 1 }}
-        viewport={{ once: false, margin: "0% 0% -20% 0%" }}
-        ref={cubesContainerRef}
-        className="relative flex-1 mb-8 flex flex-col gap-2 items-center justify-end"
-      >
-        {cubesContainerSize.height >= 128 && (
-          <>
-            <div className="relative w-px bg-dark-1 h-16 opacity-60">
-              <motion.span
-                initial={{ y: "4px" }}
-                animate={{
-                  y: [
-                    "4px",
-                    "48px",
-                    "40px",
-                    "48px",
-                    "44px",
-                    "48px",
-                    "48px",
-                    "32px",
-                    "24px",
-                    "16px",
-                    "8px",
-                    "4px",
-                  ],
-                }}
-                transition={{
-                  ease: "linear",
-                  duration: 1,
-                  delay: 1,
-                  repeat: Infinity,
-                  repeatType: "loop",
-                  repeatDelay: 2,
-                }}
-                className="rounded-full bg-dark-2 border border-dark-1 size-3 absolute left-1/2 -translate-x-1/2 hover:animate-pulse"
-              />
-            </div>
-          </>
+      <div
+        className={cn(
+          "grid [@container(min-aspect-ratio:34/21)]:h-full [@container(max-aspect-ratio:34/21)]:w-full",
+          "aspect-21/34 grid-rows-[8fr_1fr_1fr_3fr_21fr] grid-cols-[5fr_1fr_2fr_13fr]",
+          "lg:aspect-34/21 lg:grid-cols-[21fr_3fr_1fr_1fr_8fr] lg:grid-rows-[5fr_1fr_2fr_13fr]"
         )}
-      </motion.div>
+      >
+        {/* <div className="grid col-span-full row-span-full grid-cols-subgrid grid-rows-subgrid">
+          <span
+            className={cn(
+              "bg-red-300",
+              "row-start-5 col-start-1 row-end-6 col-end-5",
+              "lg:row-start-1 lg:col-start-1 lg:row-end-5 lg:col-end-2"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-orange-300",
+              "row-start-1 col-start-4 row-end-5 col-end-5",
+              "lg:row-start-4 lg:col-start-2 lg:row-end-5 lg:col-end-6"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-teal-300",
+              "row-start-1 col-start-1 row-end-2 col-end-4",
+              "lg:row-start-1 lg:col-start-5 lg:row-end-4 lg:col-end-6"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-green-300",
+              "row-start-2 col-start-1 row-end-5 col-end-2",
+              "lg:row-start-1 lg:col-start-2 lg:row-end-2 lg:col-end-5"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-cyan-300",
+              "row-start-4 col-start-2 row-end-5 col-end-4",
+              "lg:row-start-2 lg:col-start-2 lg:row-end-4 lg:col-end-3"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-amber-300",
+              "row-start-2 col-start-3 row-end-4 col-end-4",
+              "lg:row-start-3 lg:col-start-3 lg:row-end-4 lg:col-end-5"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-emerald-300",
+              "row-start-2 col-start-2 row-end-3 col-end-3",
+              "lg:row-start-2 lg:col-start-3 lg:row-end-3 lg:col-end-4"
+            )}
+          />
+          <span
+            className={cn(
+              "bg-blue-300",
+              "row-start-3 col-start-2 row-end-4 col-end-3",
+              "lg:row-start-2 lg:col-start-4 lg:row-end-3 lg:col-end-5"
+            )}
+          />
+        </div> */}
+
+        <div className="grid col-span-full row-span-full grid-cols-subgrid grid-rows-subgrid">
+          <div
+            className={cn(
+              "row-start-5 col-start-1 row-end-6 col-end-5",
+              "lg:row-start-1 lg:col-start-1 lg:row-end-5 lg:col-end-2"
+            )}
+          ></div>
+          <div className="row-start-1 col-start-2 row-end-2 col-end-6 flex items-center justify-center">
+            <h1 className="text-5xl font-think-loved">EXPERIENCE</h1>
+          </div>
+          <div className="row-start-2 col-start-2 row-end-5 col-end-6 flex flex-col gap-16">
+            <ExperienceItem
+              imgSrc="assets/logos/JMAN Group.png"
+              imgAlt="JMAN Group"
+              imgClassName="p-2 [&>img]:drop-shadow-xl"
+              title="Software Developer"
+              company="JMAN Group Pvt.Ltd."
+              url="https://jmangroup.com/"
+              location="Chennai, India (On-site)"
+              period="Jul/2024 - Sep/2025"
+              description={[
+                "Built a skills tracking platform enabling employees to register skills and project managers to identify suitable team members for ongoing projects.",
+                "Developed the frontend for an AI-powered product using modern web technologies, with a Python backend.",
+                "Automated Single Customer View (SCV) using Databricks, PySpark, and Splink, improving data unification across multiple CRMs.",
+                "Enhanced and maintained a Data Cube application with SQL backend and Power BI frontend, optimizing performance and supporting a client during a major exit process.",
+                "Collaborated in a fast-paced environment to fix legacy bugs, integrate new client features, and deliver consistent value across project phases.",
+              ]}
+            />
+
+            <ExperienceItem
+              imgSrc="assets/logos/IDZ Digital.png"
+              imgAlt="IDZ Digital"
+              imgClassName="p-4 [&>img]:drop-shadow-xl"
+              title="Game Developer"
+              company="IDZ Digital Pvt.Ltd."
+              url="https://www.idzdigital.com/"
+              location="Mumbai, India (Remote)"
+              period="Jun/2023 - Oct/2023"
+              description={[
+                "Developed and optimized ragdoll and rope physics systems for Unity, enabling smooth simulation and rendering of multiple physics segments.",
+                "Improved understanding of Unity's physics engine and C# scripting while collaborating in a distributed team.",
+                "Built a 3D projection tool for painting and interacting with 3D objects via 2D screen input.",
+                "Strengthened communication and teamwork skills through remote collaboration.",
+              ]}
+            />
+          </div>
+        </div>
+      </div>
     </motion.section>
   );
 });
