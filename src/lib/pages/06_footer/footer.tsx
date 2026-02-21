b4b5166c05f655f43e7ff28fef0c8a9bf4d897e7
Aniket Das <dasaniket20002@gmail.com>
2026-02-21 16:14:08 +0530

    deleted older files for build

--- src/lib/pages/06_footer/footer.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "../../utils";
-
-export default function Footer({ className }: { className?: string }) {
-  return (
-    <footer className={cn("h-[50dvh] w-full bg-dark-1", className)}></footer>
-  );
-}
